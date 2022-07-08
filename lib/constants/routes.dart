fa5bb3d72f470d9ee81dd30f616e2590e40bc57f
Jose Salvini <jsalvini@tipre.com>
2022-07-08 09:30:51 -0300

    Step-17

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,4 +2,4 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verify-email/';
-const newNoteRoute = '/note/new-note/';
+const createOrUpdateNoteRoute = '/note/new-note/';
