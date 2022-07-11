2b013c52b44b7625363ca698128c8eb4f22a50b6
Jose Salvini <jsalvini@tipre.com>
2022-07-11 12:07:25 -0300

    Step-22

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes/create_update_note_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
@@ -28,6 +29,40 @@ void main() {
   );
 }
 
+// class HomeView extends StatefulWidget {
+//   const HomeView({Key? key}) : super(key: key);
+
+//   @override
+//   State<HomeView> createState() => _HomeViewState();
+// }
+
+// class _HomeViewState extends State<HomeView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//       future: AuthService.firebase().initialize(),
+//       builder: ((context, snapshot) {
+//         switch (snapshot.connectionState) {
+//           //Si la conexion esta bien cargo la pantalla
+//           case ConnectionState.done:
+//             final user = AuthService.firebase().currentUser;
+//             if (user != null) {
+//               if (user.isEmailVerified) {
+//                 return const NotesView();
+//               } else {
+//                 return const VerifyEmailView();
+//               }
+//             } else {
+//               return const LoginView();
+//             }
+//           default:
+//             return const CircularProgressIndicator();
+//         }
+//       }),
+//     );
+//   }
+// }
+
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
@@ -36,28 +71,135 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: AuthService.firebase().initialize(),
-      builder: ((context, snapshot) {
-        switch (snapshot.connectionState) {
-          //Si la conexion esta bien cargo la pantalla
-          case ConnectionState.done:
-            final user = AuthService.firebase().currentUser;
-            if (user != null) {
-              if (user.isEmailVerified) {
-                return const NotesView();
-              } else {
-                return const VerifyEmailView();
-              }
-            } else {
-              return const LoginView();
-            }
-          default:
-            return const CircularProgressIndicator();
-        }
-      }),
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bloc'),
+        ),
+        body: BlocConsumer<CounterBloc, CounterState>(
+          listener: (context, state) {
+            _textController.clear();
+          },
+          builder: (context, state) {
+            final invalidValue =
+                (state is CounterStateInvalid) ? state.invalidValue : '';
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text('Valor ingresado => ${state.value}'),
+                  Visibility(
+                    visible: state is CounterStateInvalid,
+                    child: Text('Valor invalido: $invalidValue'),
+                  ),
+                  TextField(
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                      hintText: 'Ingrese un número aqui',
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          context
+                              .read<CounterBloc>()
+                              .add(DecrementEvent(_textController.text));
+                        },
+                        child: const Text('-'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          context
+                              .read<CounterBloc>()
+                              .add(IncrementEvent(_textController.text));
+                        },
+                        child: const Text('+'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
+  }
+}
+
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(int value) : super(value);
+}
+
+class CounterStateInvalid extends CounterState {
+  final String invalidValue;
+  const CounterStateInvalid({
+    required this.invalidValue,
+    required int previousValue,
+  }) : super(previousValue);
+}
+
+@immutable
+abstract class CounterEvent {
+  final String value;
+  const CounterEvent(this.value);
+}
+
+class IncrementEvent extends CounterEvent {
+  const IncrementEvent(String value) : super(value);
+}
+
+class DecrementEvent extends CounterEvent {
+  const DecrementEvent(String value) : super(value);
+}
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterStateValid(0)) {
+    on<IncrementEvent>((event, emit) {
+      final integerValue = int.tryParse(event.value);
+      if (integerValue == null) {
+        emit(CounterStateInvalid(
+          invalidValue: event.value,
+          previousValue: state.value,
+        ));
+      } else {
+        emit(CounterStateValid(state.value + integerValue));
+      }
+    });
+    on<DecrementEvent>((event, emit) {
+      final integerValue = int.tryParse(event.value);
+      if (integerValue == null) {
+        emit(CounterStateInvalid(
+          invalidValue: event.value,
+          previousValue: state.value,
+        ));
+      } else {
+        emit(CounterStateValid(state.value - integerValue));
+      }
+    });
   }
 }
